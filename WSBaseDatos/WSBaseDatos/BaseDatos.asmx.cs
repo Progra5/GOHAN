fa16aa7214d93f05c7ead0d828737491d09db2e8
joelalex207 <36746236+joelalex207@users.noreply.github.com>
2018-02-25 13:52:39 -0600

    Capa base de datos

---
+++ WSBaseDatos/WSBaseDatos/BaseDatos.asmx.cs
@@ -0,0 +1,190 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Web;
+using System.Web.Services;
+
+namespace WSBaseDatos
+{
+    /// <summary>
+    /// Descripción breve de BaseDatos
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class BaseDatos : System.Web.Services.WebService
+    {
+        public static string Conexion = @"workstation id=Androide17.mssql.somee.com;packet size=4096;user id=alex207_SQLLogin_1;pwd=lif58eecxe;data source=Androide17.mssql.somee.com;persist security info=False;initial catalog=Androide17";
+        SqlConnection cn = new SqlConnection(Conexion);
+        SqlCommand comando = new SqlCommand();
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public string InsertarCuerpo(string nombre, string descubridor, byte[] archivo)
+        {
+            try //protejemos la consulta
+            {
+                using (SqlConnection cn = new SqlConnection(Conexion))
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("INSTIP", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@_Nombre", nombre);
+                    cmd.Parameters.AddWithValue("@_Descubridor", descubridor);
+                    cmd.Parameters.AddWithValue("@_FOTOGRAFIA", archivo);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                comando.Dispose();
+                return 0;
+            }
+        }
+
+        [WebMethod]
+        public string insertarTipo(string descripcion)
+        {
+            try //protejemos la consulta
+            {
+                using (SqlConnection cn = new SqlConnection(Conexion))
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("INSTIP", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@DESCRIPCION", descripcion);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                comando.Dispose();
+                return ex.ToString();
+            }
+        }
+
+        [WebMethod]
+        public string ConsultaCuerpos()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (cn = new SqlConnection(Conexion))
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("SELCUE", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    cn.Close();
+
+                    string txt = Newtonsoft.Json.JsonConvert.SerializeObject(dt);
+
+                    
+                    
+                    return txt;
+                }
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                return "";
+            }
+        }
+
+        [WebMethod]
+        public string consultarTipos()
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (cn = new SqlConnection(Conexion))
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("SELTIP", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    da.Fill(dt);
+                    cn.Close();
+                    string txt = Newtonsoft.Json.JsonConvert.SerializeObject(dt);
+
+                    /*for (int i = 0; i < dt.Rows.Count; i++)
+                    {
+                        string txtCol = "";
+                        for (int j = 0; j < dt.Columns.Count; j++)
+                        {
+                            if (j == 0)
+                            {
+                                txtCol = dt.Rows[i][j].ToString();
+                            }
+                            else
+                            {
+                                txtCol = txtCol + "," + dt.Rows[i][j].ToString();
+                            }
+                        }
+                        if (i == 0)
+                        {
+                            txt = txtCol;
+                        }
+                        else
+                        {
+                            txt = txt + "+" + txtCol;
+                        }
+                    }*/
+
+                    return txt;
+                }
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                return "";
+            }
+        }
+
+        [WebMethod]
+        public string InsertarAsociados(int idCuerpo, int idAsociado, int idtipo)
+        {
+            try //protejemos la consulta
+            {
+                using (SqlConnection cn = new SqlConnection(Conexion))
+                {
+                    cn.Open();
+                    SqlCommand cmd = new SqlCommand("INSASO", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@ID_CUERPO", idCuerpo);
+                    cmd.Parameters.AddWithValue("@ID_ASOCIADO", idAsociado);
+                    cmd.Parameters.AddWithValue("@ID_TIPO", idtipo);
+                    cmd.ExecuteNonQuery();
+                    cn.Close();
+                }
+                return "";
+            }
+            catch (Exception ex)
+            {
+                cn.Close();
+                comando.Dispose();
+                return ex.ToString();
+            }
+        }
+
+    }
+}
